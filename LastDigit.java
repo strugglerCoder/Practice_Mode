cb87ee6559d854e5c7e58dd02aaabed834ee6235
Strugglercoder <68840546+Strugglercoder@users.noreply.github.com>
2021-01-13 19:09:40 +0530

    Find last number in a digit

---
+++ LastDigit.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * LastDigit
+ */
+public class LastDigit {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+        int n = sc.nextInt();
+
+        int lst = n%10;
+
+        System.out.println(lst);
+    }
+}
